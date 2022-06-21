273c6138175d298baa0dedb94d1db20e45b918db
Mario Ortega <mortegagarcia@ucmerced.edu>
2022-06-21 14:54:00 -0700

    Added back setWasmPath to avoid error

--- webui/src/ModelLoader.js
+++ webui/src/ModelLoader.js
@@ -5,6 +5,9 @@ const modelEndPoint = `http://localhost:3009/get_model`
 
 export default class ModelLoader {
     async loadModel() {
+        tflite.setWasmPath(
+            'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-tflite@0.0.1-alpha.8/dist/'
+         );
         const model = await tflite.loadTFLiteModel(modelEndPoint);
         console.log(model)
         return model;
