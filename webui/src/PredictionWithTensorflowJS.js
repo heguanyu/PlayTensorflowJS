cea8405ff05396baedd7b91e4756c5e000355ec6
Deblina Das <dedas@dedas-mn1.linkedin.biz>
2022-06-22 15:11:27 -0700

    pushing the code for tensorflow js model prediction

--- webui/src/PredictionWithTensorflowJS.js
+++ webui/src/PredictionWithTensorflowJS.js
@@ -1,13 +1,26 @@
-import tfjs from '@tensorflow/tfjs'
+import * as tfjs from "@tensorflow/tfjs"
 
 export default class PredictionWithTensorflowJS {
-    predict(model, featureNames, features) {
+    predict(model, featureNames, data) {
         const startTime = Date.now()
-        // TODO: Use TFJS to predict the model vs features
-
+        // Prepare input tensors using feature names.
+        let arr = new Array();
+        for (let i = 0; i < featureNames.length; i++) {
+            if (data.features.hasOwnProperty(featureNames[i]))
+            {
+                arr.push(data.features[featureNames[i]]);
+            }
+            else {
+                arr.push(0.0);
+            }
+        }
+        const inputTensor =  tfjs.tensor([arr]);
+        // Run inference and get output tensors.
+        let outputTensor = model.predict(inputTensor);
         const endTime = Date.now();
         return {
-            score: 0.0,
+            //Rounding to 8 decimal places
+            score: Math.round((outputTensor.dataSync()[0] + Number.EPSILON) * 100000000) / 100000000 ,
             time: endTime-startTime
         };
     }
